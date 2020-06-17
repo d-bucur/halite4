d2c26b0bce91f6e6055c280f6c55224848863bc3
Dorian <sirdorius@gmail.com>
2020-06-17 22:48:29 +0200

    added visualization to notebook

--- submission.py
+++ submission.py
@@ -1,12 +1,25 @@
 from kaggle_environments.envs.halite.helpers import *
+from numpy import array
+
+
+class AttributeDict(dict):
+    __getattr__ = dict.__getitem__
+    __setattr__ = dict.__setitem__
+
+
+internals = AttributeDict()
 
 
 def agent(obs, config):
+    global internals
+
     board = Board(obs, config)
     obs = Observation(obs)
     config = Configuration(config)
-    #print(board)
     me = board.current_player
+    internals.halite_map = array(obs.halite).reshape((config.size, config.size))
+
+    #print(board)
     #print(board.cells)
 
     # Set actions for each ship
