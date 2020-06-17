2af39164f9aa1c37ad4c969c8e0f9d8e3cb54472
Dorian <sirdorius@gmail.com>
2020-06-18 00:17:42 +0200

    work on threatmaps in notebook

--- submission.py
+++ submission.py
@@ -1,5 +1,6 @@
 from kaggle_environments.envs.halite.helpers import *
-from numpy import array
+import numpy as np
+from scipy.ndimage import gaussian_filter
 
 
 class AttributeDict(dict):
@@ -17,10 +18,14 @@ def agent(obs, config):
     obs = Observation(obs)
     config = Configuration(config)
     me = board.current_player
-    internals.halite_map = array(obs.halite).reshape((config.size, config.size))
 
-    #print(board)
-    #print(board.cells)
+    internals.halite_map = np.array(obs.halite).reshape((config.size, config.size))
+    # internals.halite_heat = gaussian_filter(internals.halite_map, sigma=1)
+    internals.threat_map = np.zeros((config.size, config.size))
+    for ship in board.ships.values():
+        if ship.player_id != board.current_player_id:
+            internals.threat_map[ship.position] = -10000
+
 
     # Set actions for each ship
     for ship in me.ships:
