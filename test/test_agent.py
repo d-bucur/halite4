e6200486fbf67be82f828a1507f2a3f753f10be2
Dorian <sirdorius@gmail.com>
2020-06-21 19:42:08 +0200

    hacky solution for test

--- test/test_agent.py
+++ test/test_agent.py
@@ -1,11 +1,9 @@
-import pytest
 from kaggle_environments import make
 
 from submission import agent
 
 
-@pytest.mark.skip("does not check for bot internal errors")
 def test_agent_completes():
     env = make("halite", configuration={"episodeSteps": 400}, debug=True)
-    env.run([agent, "submission.py", "submission.py", "submission.py"])
-    assert env.done
+    env.run([agent, agent])
+    assert not any(step[0].status == 'ERROR' for step in env.steps)
