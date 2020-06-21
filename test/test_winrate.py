05d096b752d99fa0f9fc90642fa67d357fff63ae
Dorian <sirdorius@gmail.com>
2020-06-21 16:40:19 +0200

    changed winrate tests

--- test/test_winrate.py
+++ test/test_winrate.py
@@ -1,8 +1,10 @@
+import pytest
 from kaggle_environments import evaluate
 
 from submission import agent
 
 
+@pytest.mark.skip
 def test_wins_against_random():
     my_score, enemy_score = evaluate(
         "halite",
@@ -10,3 +12,13 @@ def test_wins_against_random():
         num_episodes=1, configuration={"agentExec": "LOCAL"}
     )[0]
     assert my_score > enemy_score
+
+
+@pytest.mark.skip
+def test_wins_against_4_randoms():
+    scores = evaluate(
+        "halite",
+        [agent, "random", "random", "random"],
+        num_episodes=1, configuration={"agentExec": "LOCAL"}
+    )
+    assert scores[0] == max(scores)
