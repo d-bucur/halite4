d3d16892f3c963b083cc96b46ed4182ebe756b79
Dorian <sirdorius@gmail.com>
2020-06-21 15:52:01 +0200

    added logic to build the first shipyard in the best position

--- submission.py
+++ submission.py
@@ -1,3 +1,5 @@
+import abc
+
 from kaggle_environments.envs.halite.helpers import *
 import numpy as np
 from scipy.ndimage import gaussian_filter
@@ -8,41 +10,130 @@ class AttributeDict(dict):
     __setattr__ = dict.__setitem__
 
 
-_vars = AttributeDict()
+'''
+Coordinate system
+  0      y
+    +----->
+    |
+  x |
+    v
+'''
+SimplePoint = Tuple[int, int]  # TODO subclass from Point
 
 
-def internals():
-    # helper method to expose data to notebook reliably with autoreload
-    return _vars
+_vars = AttributeDict()  # TODO proper types
+_vars.orders = {}
+saved_config: Optional[Configuration] = None
+
+
+class ShipOrder:
+    @abc.abstractmethod
+    def execute(self, ship: Ship):
+        ...
+
+
+class BuildShipyardOrder(ShipOrder):
+    def __init__(self, target: SimplePoint):
+        self.target = target
+
+    def execute(self, ship: Ship):
+        ship_pos = ship.position.norm
+        if ship_pos == self.target:
+            return ShipAction.CONVERT
+        else:
+            return path_to_next(ship_pos, self.target)
+
+    def is_done(self, ship: Ship):
+        ship_pos = ship.position.norm
+        return ship_pos == self.target and ship.cell.shipyard
+
+    def __repr__(self) -> str:
+        return f"Build shipyard at {self.target}"
+
+
+def path_to_next(start: SimplePoint, target: SimplePoint) -> ShipAction:
+    # TODO wraparound map
+    tx, ty = target
+    sx, sy = start
+    dx = tx - sx
+    dy = ty - sy
+    # TODO move through diagonal or pathfind through heatmap
+    if dx < 0:
+        return ShipAction.NORTH
+    elif dx > 0:
+        return ShipAction.SOUTH
+    elif dy < 0:
+        return ShipAction.WEST
+    else:
+        return ShipAction.EAST
 
 
 def agent(obs, config):
     global _vars
+    global saved_config
     # add method to convert coordinate system back to topleft 0,0
     Point.norm = property(lambda self: (config.size - 1 - self.y, self.x))
 
     board = Board(obs, config)
     obs = Observation(obs)
-    config = Configuration(config)
+    if not saved_config:
+        saved_config = Configuration(config)
     me = board.current_player
 
-    _vars.halite_map = np.array(obs.halite).reshape((config.size, config.size))
-    _vars.halite_map = gaussian_filter(_vars.halite_map, sigma=1, mode='wrap')
+    calc_maps(board, config, obs)
 
-    _vars.threat_map = np.zeros((config.size, config.size))
-    for ship in board.ships.values():
-        if ship.player_id != board.current_player_id:
-            _vars.threat_map[ship.position.norm] = -5000
-    _vars.threat_map = gaussian_filter(_vars.threat_map, sigma=1.2, mode='wrap')
+    if len(me.shipyards) == 0:
+        first_ship = me.ships[0]
+        target = calc_highest_in_range(_vars.reward_map, first_ship.position.norm, 4)
+        if first_ship.id not in _vars.orders:
+            _vars.orders[first_ship.id] = BuildShipyardOrder(target)
 
-    _vars.reward_map = _vars.halite_map * 3 + _vars.threat_map
-
-    # Set actions for each ship
-    for ship in me.ships:
-        ship.next_action = ShipAction.NORTH
+    build_ship_actions(me.ships, board.ships)
 
     # Set actions for each shipyard
     for shipyard in me.shipyards:
         shipyard.next_action = None
 
     return me.next_actions
+
+
+def distance(p1: SimplePoint, p2: SimplePoint) -> int:
+    ax, ay = p1
+    bx, by = p2
+    return abs(bx-ax) + abs(by-ay)
+
+
+def calc_highest_in_range(arr: np.ndarray, position: SimplePoint, max_distance: int) -> SimplePoint:
+    pos_max = None
+    val_max = float('-inf')
+    for idx, val in np.ndenumerate(arr):
+        if distance(position, idx) < max_distance:
+            if val_max is None or val_max < val:
+                pos_max = idx
+                val_max = val
+    return pos_max
+
+
+def calc_maps(board, config, obs) -> None:
+    global _vars
+    _vars.halite_map = np.array(obs.halite).reshape((config.size, config.size))
+    _vars.halite_map = gaussian_filter(_vars.halite_map, sigma=1, mode='wrap')
+    _vars.threat_map = np.zeros((config.size, config.size))
+    for ship in board.ships.values():
+        if ship.player_id != board.current_player_id:
+            _vars.threat_map[ship.position.norm] = -5000
+    _vars.threat_map = gaussian_filter(_vars.threat_map, sigma=1.2, mode='wrap')
+    _vars.reward_map = _vars.halite_map * 3 + _vars.threat_map
+
+
+def build_ship_actions(ships: List[Ship], ships_dict: Dict[str, Ship]) -> None:
+    for ship_id in list(_vars.orders.keys()):
+        if ship_id not in ships_dict or _vars.orders[ship_id].is_done(ships_dict[ship_id]):
+            del _vars.orders[ship_id]
+    for ship in ships:
+        ship.next_action = _vars.orders[ship.id].execute(ship)
+
+
+def internals():
+    # helper method to expose data to notebook reliably with autoreload
+    return _vars
