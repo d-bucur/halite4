7cf2721cc70008b34b54d102198ba438be6475d1
Dorian <sirdorius@gmail.com>
2020-06-25 22:17:21 +0200

    restructure maps

--- src/commander.py
+++ src/commander.py
@@ -3,37 +3,41 @@ from kaggle_environments.envs.halite.helpers import Configuration
 from scipy.ndimage import gaussian_filter
 
 from src.gamestate import GameState
+from src.maps import AttractionMap
 
 
 class Commander:
-
     def __init__(self, configuration: Configuration):
         GameState.config = configuration
 
-        self.halite_map: np.ndarray = None
-        self.expansion_map: np.ndarray = None
-        self.threat_map: np.ndarray = None
-        self.reward_map: np.ndarray = None
+        self.halite_map: AttractionMap = None
+        self.expansion_map: AttractionMap = None
+        self.threat_map: AttractionMap = None
+        self.reward_map: AttractionMap = None
         
     def get_next_actions(self):
         self.calc_maps()
         return GameState.board.current_player.next_actions
 
     def calc_maps(self) -> None:
-        self.halite_map = np.array(GameState.halite).reshape(self._map_size())
+        halite_map = np.array(GameState.halite).reshape(self._map_size())
+        self.halite_map = AttractionMap(halite_map)
 
-        self.expansion_map = np.copy(self.halite_map)
+        expansion_map = np.copy(halite_map)
         for base in GameState.board.shipyards.values():
-            self.expansion_map[base.position.norm] = -5000
-        self.expansion_map = gaussian_filter(self.expansion_map, sigma=3, mode='wrap')
+            expansion_map[base.position.norm] = -5000
+        expansion_map = gaussian_filter(expansion_map, sigma=3, mode='wrap')
+        self.expansion_map = AttractionMap(expansion_map)
 
-        self.threat_map = np.zeros(self._map_size())
+        threat_map = np.zeros(self._map_size())
         for ship in GameState.board.ships.values():
             if ship.player_id != GameState.board.current_player_id:
-                self.threat_map[ship.position.norm] = -5000
-        self.threat_map = gaussian_filter(self.threat_map, sigma=1.2, mode='wrap')
+                threat_map[ship.position.norm] = -5000
+        threat_map = gaussian_filter(threat_map, sigma=1.2, mode='wrap')
+        self.threat_map = AttractionMap(threat_map)
 
-        self.reward_map = self.halite_map + self.threat_map
+        reward_map = halite_map + threat_map
+        self.halite_map = AttractionMap(reward_map)
 
     @staticmethod
     def _turns_remaining():
