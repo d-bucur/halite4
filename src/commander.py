fixed bug in gradient calculation

src/commander.py
@@ -60,7 +60,7 @@ class Strategies:
 
         return_halite = np.zeros(GameState.map_size())
         for base in GameState.board.current_player.shipyards:
-            return_halite[base.position.norm] = 3000
+            return_halite[base.position.norm] = 6000
         return_halite = gaussian_filter(return_halite, sigma=3, mode='wrap')
         Strategies.return_halite = AttractionMap(return_halite)
 
@@ -83,39 +83,42 @@ class Commander:
         else:
             Strategies.expand.priority = 0
         Strategies.mine_halite.priority = 1
-        Strategies.friendly_bases.priority = 0.3
+        Strategies.friendly_bases.priority = 0.15
         Strategies.avoid_friendlies.priority = 1
         Strategies.avoid_enemies.priority = 1
+        Strategies.return_halite.priority = 1
 
     def _make_action(self, ship):
         FORCE_CUTOFF = 5
         total_force = P(0, 0)
         total_priorities = 0
         ship_pos = ship.position.norm
-        logging.debug(f"--------- {ship}")
-        logging.debug(f"cell halite {ship.cell.halite}, ship halite {ship.halite}")
+        logging.info(f"--------- {ship}")
+        logging.info(f"cell halite {ship.cell.halite}, ship halite {ship.halite}")
 
         def add_force(name: str, f: P, prio: float):
             nonlocal total_priorities, total_force
-            logging.debug(f"{name} {f} * {prio} = {f*prio}")
+            logging.info(f"{name:16} {f*prio} = {f} * {prio:0.1f}")
             total_priorities += prio
             total_force += f * prio
 
         # Create new bases
         add_force('expand', Strategies.expand.at(ship_pos), Strategies.expand.priority)
 
+        '''
         # Get away from friendly shipyards
         # TODO only add if current action is none
         friendly_base_priority = Strategies.friendly_bases.priority
         #if ship.halite > 0:
         #    friendly_base_priority = -friendly_base_priority
         add_force('friendly base', Strategies.friendly_bases.at(ship_pos), friendly_base_priority)
+        '''
 
         # Avoid colliding into friendlies
-        AVOID_FRIENDLIES_REDUCTION_FACTOR = 100
-        avoid_friendlies_force = self._calc_friendlies_map(ship).at(ship_pos)
+        AVOID_FRIENDLIES_REDUCTION_FACTOR = 10
+        avoid_friendlies_force = self._calc_friendlies_map(ship).at(ship_pos) / AVOID_FRIENDLIES_REDUCTION_FACTOR
         avoid_friendlies_priority = Strategies.avoid_friendlies.priority
-        avoid_friendlies_priority *= avoid_friendlies_force.magnitude_squared / AVOID_FRIENDLIES_REDUCTION_FACTOR
+        avoid_friendlies_priority *= avoid_friendlies_force.magnitude / 3
         add_force(
             'avoid friendlies',
             avoid_friendlies_force,
@@ -128,20 +131,20 @@ class Commander:
         #mining_others_reduction = (MINING_CUTOFF_OTHERS - ship.cell.halite) / MINING_CUTOFF_OTHERS
         #total_priorities *= mining_others_reduction
         cell_halite_modifier = ship.cell.halite / GameState.config.max_cell_halite * 3 + 1
-        logging.debug(f'current halite priority booster = {cell_halite_modifier}')
-        carrying_halite_modifier = min(0, (MAX_HALITE_X_SHIP - ship.halite) / MAX_HALITE_X_SHIP)  # TODO limit at 0
+        logging.info(f'current halite priority booster = {cell_halite_modifier}')
+        carrying_halite_modifier = max(0, (MAX_HALITE_X_SHIP - ship.halite) / MAX_HALITE_X_SHIP)
         mine_priority = Strategies.mine_halite.priority * carrying_halite_modifier * cell_halite_modifier
         add_force('mine', Strategies.mine_halite.at(ship_pos), mine_priority)
 
         # Return halite
-        BOOST_RETURN_HALITE = 1
-        return_halite_priority = Strategies.return_halite.priority * (ship.halite / MAX_HALITE_X_SHIP) * BOOST_RETURN_HALITE
-        add_force('return halite', Strategies.mine_halite.at(ship_pos), return_halite_priority)
+        RETURN_TRESHOLD = 300
+        return_halite_priority = Strategies.return_halite.priority * (ship.halite / RETURN_TRESHOLD)
+        add_force('return halite', Strategies.return_halite.at(ship_pos), return_halite_priority)
 
         direction = total_force / total_priorities
         add_force('TOTAL', direction, 1)
         ship.next_action = action_from_force(direction, FORCE_CUTOFF)
-        logging.debug(f'Action {ship.next_action}')
+        logging.info(f'Action {ship.next_action}')
 
         if not ship.next_action and Strategies.expand.priority > 5 and self._can_build_base():
             ship.next_action = ShipAction.CONVERT

src/maps.py
@@ -23,9 +23,13 @@ class AttractionMap:
         return P(dx, dy)
 
 
-def make_field(arr: np.ndarray): # -> FieldType:
+def make_field_gradient(arr: np.ndarray) -> FieldType:
+    return np.gradient(arr)
+
+
+def make_field(arr: np.ndarray):
     # x derivate
-    dx = np.zeros(arr.shape)
+    dx = np.empty(arr.shape)
     for y in range(0, arr.shape[1]):
         curr = arr[(-2, y)]
         next = arr[(-1, y)]
@@ -35,10 +39,8 @@ def make_field(arr: np.ndarray): # -> FieldType:
             next = arr[(x + 1, y)]
             if curr >= prev and curr >= next:
                 dx[(x, y)] = 0.0
-            elif next > prev:
-                dx[(x, y)] = next - curr
-            else:  # prev > next
-                dx[(x, y)] = curr - prev
+            else:
+                dx[(x, y)] = next - prev
 
     # y derivate
     dy = np.empty(arr.shape)
@@ -51,10 +53,8 @@ def make_field(arr: np.ndarray): # -> FieldType:
             next = arr[(x, y + 1)]
             if curr >= prev and curr >= next:
                 dy[(x, y)] = 0.0
-            elif next > prev:
-                dy[(x, y)] = next - curr
-            else:  # prev > next
-                dy[(x, y)] = curr - prev
+            else:
+                dy[(x, y)] = next - prev
     return [dx, dy]
 
 